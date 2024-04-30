5fc93f709770b45ffb76a006352dc5aa15dc7bc9
Hun <dudgns9194@gmail.com>
2024-04-30 17:16:45 +0900

    PUT 수정기능 추가

--- youtuber-demo.js
+++ youtuber-demo.js
@@ -75,7 +75,7 @@ app.delete('/youtubers', (req, res) => {
     } else {
         msg = '삭제할 유튜버가 없습니다.'
     }
-    res.json({message : msg});
+    res.json({ message: msg });
 });
 
 app.delete('/youtubers/:id', (req, res) => {
@@ -91,6 +91,24 @@ app.delete('/youtubers/:id', (req, res) => {
         db.delete(id);
         res.json({
             message: `${channelTitle}님, 아쉽지만 우리 인연은 여기까지 인가요...`
+        });
+    }
+});
+
+app.put('/youtubers/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    var youtuber = db.get(id);
+    var oldTitle = youtuber.channelTitle;
+    if (youtuber === undefined) {
+        res.json({
+            message: `요청하신${d}번은 없는 유튜버입니다.`
+        });
+    } else {
+        youtuber.channelTitle = req.body.channelTitle;
+        db.set(id, youtuber);
+
+        res.json({
+            message: `${oldTitle}님, 채널명이 ${youtuber.channelTitle}로 수정되었습니다.`
         });
     }
 });
