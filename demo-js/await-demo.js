50df48535b56d02d5ad13a5d575181af4a414db0
Hun <dudgns9194@gmail.com>
2024-05-30 16:23:49 +0900

    promise 순서대로 실행

--- demo-js/await-demo.js
+++ demo-js/await-demo.js
@@ -10,12 +10,28 @@
 // await이.. Promise 객체.then() 이 메소드를 좀 더 쉽게 사용할 수 있는 방법
 
 async function f() {
-    let promise = new Promise(function(resolve, reject){
-        setTimeout(()=>resolve('완료!'), 3000);
+
+    // promise 객체 한개당 => query 하나
+    let promise1 = new Promise(function (resolve, reject) {
+        setTimeout(() => resolve('첫번째 쿼리'), 3000);
     });
 
-    let result = await promise;
-    console.log(result);
+    let result1 = await promise1;
+    console.log(result1);
+
+    let promise2 = new Promise(function (resolve, reject) {
+        setTimeout(() => resolve('두번째 쿼리 with ' + result1), 3000);
+    });
+
+    let result2 = await promise2;
+    console.log(result2);
+
+    let promise3 = new Promise(function (resolve, reject) {
+        setTimeout(() => resolve('세번째 쿼리 with ' + result2), 3000);
+    });
+
+    let result3 = await promise3;
+    console.log(result3);
 }
 
 f();
