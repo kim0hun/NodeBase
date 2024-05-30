347b7e57c5e467dde2eb956fd4a0bc089c8d383a
Hun <dudgns9194@gmail.com>
2024-05-30 15:30:06 +0900

    Promise 객체

---
+++ demo-js/async-await-demo.js
@@ -0,0 +1,23 @@
+// Promise "객체" : 약속을 지키는 친구
+
+let promise = new Promise(function (resolve, reject) {
+    // executor : 이 친구가 할 일
+
+    setTimeout(() => {
+        resolve("완료~~");
+    }, 3000);
+
+    // 일을 다~하면 무조건 콜백함수 resolve 또는 reject 둘 중 하나는 호출
+    // 할 일을 성공하면 resolve(결과)
+    //         실패하면 reject(에러)
+});
+
+// promise의 기본 메소드 : promise가 일 다 하고 (= 약속 다 지키고) 호출하는 함수
+promise.then(
+    function (result) {
+        console.log(result);
+    },
+    function (error) {
+        
+    }
+);
