eda525e59ba63a5bfaf4c1081386ba103f43a955
Hun <dudgns9194@gmail.com>
2024-05-30 15:47:19 +0900

    promise chaining

---
+++ demo-js/promise-chain-demo.js
@@ -0,0 +1,21 @@
+let promise = new Promise(function(resolve, reject){
+    setTimeout(()=> resolve('완료'), 3000);
+}).then(
+    function(result){
+        console.log(result);
+        return result + '!!!!!';
+    },
+    function(error){}
+).then(
+    function(result){
+        console.log(result);
+        return result + '!!!!!';
+    },
+    function(err0r){}
+).then(
+    function(result){
+        console.log(result);
+        return result;
+    },
+    function(error){}
+)
