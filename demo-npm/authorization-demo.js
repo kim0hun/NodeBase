328a7cf5702548a3d0d1c8892700fd41e5d73891
Hun <dudgns9194@gmail.com>
2024-06-03 17:41:52 +0900

    토큰 만료 테스트

--- demo-npm/authorization-demo.js
+++ demo-npm/authorization-demo.js
@@ -8,9 +8,18 @@ app.listen(3000);
 
 app.get('/jwt', function (req, res) {
 
-    let token = jwt.sign({
-        token : 'token'
-    }, process.env.PK);
+    // let token = jwt.sign({
+    //     study : 'express'
+    // }, process.env.PK);
+
+    const token = jwt.sign({
+        username : 'kim younghun'
+    }, process.env.PK, {
+        expiresIn: '1m',
+        issuer: 'admin'
+    });
+    
+    // console.log(token);
 
     res.cookie('jwt', token, {
         httpOnly: true
@@ -21,4 +30,19 @@ app.get('/jwt', function (req, res) {
 
 app.get('/jwt/decoded', function (req, res) {
     
+    if(jwt.TokenExpiredError){
+        return res.send('유효기간 만료');
+    }
+
+    const token = req.headers.authorization;
+    let decoded = jwt.verify(token, process.env.PK);
+
+    /*
+    유효기간이 지났어!
+    500 에러(가 난다고 해서 서버가 꺼지진 않았지만) 낼 게 아니고
+    예외(개발자가 생각하지 못한 에러) 처리
+    - 유효기간이 지난 토큰 => res. '로그인(인증) 세션(유지되는 상태)이 만료되었습니다. 다시 로그인하세요.
+    */
+
+    res.send(decoded);
 });
