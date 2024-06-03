c6d6c8de8cbb6926cb929c4618e50d520ffb34e4
Hun <dudgns9194@gmail.com>
2024-06-03 22:30:30 +0900

    throw 테스트

--- demo-js/try-catch-demo.js
+++ demo-js/try-catch-demo.js
@@ -2,7 +2,15 @@ let string = '{ "num1" : 1';
 
 try{
     let json = JSON.parse(string);
-    console.log(json);
+    // console.log(json);
+
+    if(!json.name){
+        console.log('입력 값에 이름이 없습니다.');
+        throw new SyntaxError();
+    }else{
+        console.log(json.name);
+    }
+    
 } catch (err){
     console.log(err.name);
     console.log(err.message);
